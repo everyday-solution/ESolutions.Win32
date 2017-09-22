aff76e94c0a8ec8cfedd9c73142d97eb8f8eda88
Tobias Mundt <ich@tobiasmundt.de>
2017-09-23 01:03:12 +0200

    Initial commit

---
+++ ESolutions.Win32/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ESolutions.Win32")]
+[assembly: AssemblyDescription("Assembly for dotnet interop with windows system libs.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Tobias Mundt - everyday Solutions")]
+[assembly: AssemblyProduct("ESolutions")]
+[assembly: AssemblyCopyright("Copyright &copy; Tobias Mundt - everyday Solutions 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b0ac5cb7-1195-44eb-a51d-83951f8b1f56")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
